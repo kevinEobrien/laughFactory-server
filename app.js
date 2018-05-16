060c546deefc4fb46835fef0ebf6e37ef2866730
KEVIN OBRIEN <kevin.eugene.obrien@gmail.com>
2018-05-16 15:49:48 -0600

    added upload routes

--- app.js
+++ app.js
@@ -7,9 +7,43 @@ const queries = require("./queries");
 const commqueries = require("./queries_comments");
 const cors = require("cors");
 const devMode = process.env.NODE_ENV !== "production";
+const multerS3 = require("multer-s3");
+const aws = require("aws-sdk");
+const multer = require("multer");
 
 app.use(cors());
 app.use(bodyParser.json());
+
+const s3 = new aws.S3({
+  apiVersion: "2006-03-01",
+  region: "us-east-1",
+  credentials: {
+    secretAccessKey: process.env.SECRET_ACCESS_KEY,
+    accessKeyId: process.env.ACCESS_KEY_ID
+  }
+});
+    
+const upload = multer({
+  storage: multerS3({
+    s3,
+    bucket: "tactbook",
+    key: (request, file, next) => {
+      next(null, `${Date.now()}_${file.originalname}`);
+    }
+  })
+});
+
+app.get("/upload", (request, response, next) => {
+  response.json({
+    message: "Testing out the upload route"
+  });
+});
+
+app.post("/upload", upload.array("image", 1), (request, response) => {
+  response.json({
+    imgUrl: `${request.files[0].location}`
+  });
+});
 
 app.get("/", (request, response, next) => {
   queries
